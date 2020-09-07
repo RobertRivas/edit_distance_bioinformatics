f28ccd76623475a5fc325400d7738607d25e3235
RobertRivas <r_rivas@csu.fullerton.edu>
2020-09-07 00:27:47 -0700

    Added react files to main repo instead of submodule.

---
+++ edit_distance_web_app/edit_distance_front_end/src/App.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+import './App.css';
+import EditDistance from "./edit_distance/edit_distance_form";
+
+
+class App extends Component{
+  render()
+  {
+    return (
+        <EditDistance />
+    );
+  }
+}
+
+export default App;
